34098a42c0cfe857c3fe02b2dfcbea39a76cd725
Silea Horia <horia.silea@gmail.com>
2025-05-05 17:47:48 +0300

    created skeleton for product component

--- Labs/Lab7/src/app/products/products.component.ts
+++ Labs/Lab7/src/app/products/products.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product';
+import { NgFor } from '@angular/common';
 
 @Component({
-  selector: 'app-products',
-  imports: [],
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+    selector: 'app-products',
+    imports: [NgFor],
+    templateUrl: './products.component.html',
+    styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+    products : Product[] = [];
 
+    constructor() {}
+
+    ngOnInit(): void {
+        
+    }
 }
