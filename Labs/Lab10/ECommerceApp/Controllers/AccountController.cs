0640fe2be88806c804ab2d318bba77e4253ad0dd
Silea Horia <horia.silea@gmail.com>
2025-05-21 12:03:44 +0300

    Create AccountController.cs

---
+++ Labs/Lab10/ECommerceApp/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using ECommerceApp.DataAbstractionLayer;
+using ECommerceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            DAL dal = new DAL();
+            User? user = dal.GetUser(username, password);
+
+            if (user != null)
+            {
+                HttpContext.Session.SetString("username", user.Username);
+                return RedirectToAction("Index", "Main");
+            }
+
+            ViewBag.Error = "Invalid username or password";
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
