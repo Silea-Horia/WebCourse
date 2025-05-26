b85b40d5f29c79702c6243c1c490b58cafa0c55e
Silea Horia <horia.silea@gmail.com>
2025-05-26 11:18:45 +0300

    done lab 10

--- Labs/Lab10/ECommerceApp/Controllers/AccountController.cs
+++ Labs/Lab10/ECommerceApp/Controllers/AccountController.cs
@@ -11,6 +11,12 @@ namespace ECommerceApp.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [HttpPost]
         public IActionResult Login(string username, string password)
         {
